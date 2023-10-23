c6a192a4c2cf6d0d45f890e2a1cabd5ffb7e244c
Amit Samant <amit.samant@gojek.com>
2023-10-23 22:55:25 +0530

    Initial Commit

---
+++ PiBeingIrrational/PiBeingIrrationalApp.swift
@@ -0,0 +1,17 @@
+//
+//  PiBeingIrrationalApp.swift
+//  PiBeingIrrational
+//
+//  Created by Amit Samant on 23/10/23.
+//
+
+import SwiftUI
+
+@main
+struct PiBeingIrrationalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
